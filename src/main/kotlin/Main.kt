531cc1050fc9f1d1b25e6225b4ba9568d4b1a416
dyakeso <dias.shamenovv@gmail.com>
2023-11-08 00:51:45 +0600

    third commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,8 +13,9 @@ fun main () {
 
 03.11.2023
 --VAR, VAL, INTEGER, BOOLEAN
+
 fun main() {
-    val name = "Dias"
+    var name = "Dias"
     var age = 24
 
     println("Меня зовут $name и мне $age года")
@@ -47,4 +48,41 @@ fun main() {
     println("x: $x")
     println("x + y: ${x + y")
     println("x + y + z: ${x + y + z}")
+}
+
+
+--DECIMAL NUMBERS
+Задание: Найти объем сферы
+fun main() {
+    val radius = 7.3
+    val pi = 3.14159265359
+    val volume = 0.75 * pi * radius * radius * radius
+
+    println("The volume of the sphere with the radius $radius is $volume")
+}
+
+
+--STRINGS
+Задание: Объявить переменную со строкой и напечатать ее наоборот в верхнем
+регистре
+fun main() {
+    val myName = "Dias"
+
+    println(myName.toUpperCase().reversed())
+}
+
+--LOGICAL AND COMPARISON OPERATORS
+Задание: Определить что будет возвращено true или false
+fun main() {
+    val simpleExpression = 3 > 4 || 4 > 3 && 4 <= 4
+    //Возвращено true
+
+    val bool = true
+    val x = 9
+    val y = 3
+    val z = 9
+    val hardExpression = !(x != z) && bool || z > (x + y) && (!bool
+            || y < z)
+    println(hardExpression)
+
 }*/
