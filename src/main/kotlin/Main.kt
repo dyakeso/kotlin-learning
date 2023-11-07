1035791b9f880fec74393920f64460747a3332f7
dyakeso <dias.shamenovv@gmail.com>
2023-11-07 23:28:21 +0600

    Second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 /*
-01.07.2023
+01.11.2023
 --SETTING UP WHAT WE NEED
+
 
 --HELLO WORLD
 fun main () {
@@ -8,5 +9,42 @@ fun main () {
     println()
     println("Whats up?")
 }
-*/
 
+
+03.11.2023
+--VAR, VAL, INTEGER, BOOLEAN
+fun main() {
+    val name = "Dias"
+    var age = 24
+
+    println("Меня зовут $name и мне $age года")
+
+    age = 25
+    println("В следующем году мне исполнится $age")
+
+}
+
+fun main() {
+    val age:Int = 7 + 5
+
+    println(age)
+}
+
+fun main() {
+    val x:Boolean = true
+
+    println(x)
+}
+
+
+--ARITHMETIC OPERATORS
+Задание:
+fun main() {
+    val x = 3
+    val y = 4
+    val z = 5
+
+    println("x: $x")
+    println("x + y: ${x + y")
+    println("x + y + z: ${x + y + z}")
+}*/
